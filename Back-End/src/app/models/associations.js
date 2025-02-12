4dd73ddcb810fdc213917815ca7989515c928235
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2025-02-12 17:13:50 -0300

    feat: adicionar relacionamento entre Usuário, Quarto e Reserva com onDelete CASCADE

--- Back-End/src/app/models/associations.js
+++ Back-End/src/app/models/associations.js
@@ -2,10 +2,14 @@ import usuario from "./usuario.js";
 import quarto from "./quarto.js";
 import reserva from "./reserva.js";
 
+//Caso usuario ou quarto forem removidos seus relacionamentos também serão removidos
+
 //Associação do Usuario com as reservas
-usuario.hasMany(reserva, {foreignKey: 'idUsuario'});
+usuario.hasMany(reserva, {foreignKey: 'idUsuario', onDelete: 'CASCADE'});
 reserva.belongsTo(usuario, {foreignKey: 'idUsuario'});
 
 //Associação das reservas com os hotéis
-quarto.hasMany(reserva, {foreignKey: 'idQuarto'});
+quarto.hasMany(reserva, {foreignKey: 'idQuarto', onDelete: 'CASCADE'});
 reserva.belongsTo(quarto, {foreignKey: 'idQuarto'});
+
+export { usuario, quarto, reserva };
--- Back-End/src/app/models/reserva.js
+++ Back-End/src/app/models/reserva.js
@@ -1,46 +1,33 @@
 import { Sequelize, DataTypes } from 'sequelize';
 import database from '../database/db.js';
-import usuario from './usuario.js';
-import quarto from './quarto.js';
 
-const reserva = database.define('reserva',
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            allowNull: false,
-            primaryKey: true
-        },
-        dataDeEntrada: {
-            type: DataTypes.DATEONLY,
-            allowNull: false
-        },
-        dataDeSaida: {
-            type: DataTypes.DATEONLY,
-            allowNull: false
-        },
-        idUsuario:{
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references:{
-                model: usuario,
-                key: 'id'
-            }
-        },
-        idQuarto:{
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references:{
-                model: quarto,
-                key: 'id'
-            }
-        },
-        status:{
-            type: DataTypes.BOOLEAN,
-            allowNull: false
-        }
+const reserva = database.define('reserva', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    dataDeEntrada: {
+        type: DataTypes.DATEONLY,
+        allowNull: false
+    },
+    dataDeSaida: {
+        type: DataTypes.DATEONLY,
+        allowNull: false
+    },
+    idUsuario: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    idQuarto: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    status: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
     }
-);
+});
 
 export default reserva;
-
