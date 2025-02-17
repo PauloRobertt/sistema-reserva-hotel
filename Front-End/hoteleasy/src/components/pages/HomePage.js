08da3ca8f65d702a99eb84406e71db19cc6a21b6
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2025-02-17 16:21:02 -0300

    style(front-end): estilizacao da homepage com banner

--- Front-End/hoteleasy/src/components/pages/HomePage.js
+++ Front-End/hoteleasy/src/components/pages/HomePage.js
@@ -1,9 +1,14 @@
 import Header from "../layout/Header.js"
+import styles from './HomePage.module.css';
 
 export default function HomePage(){
     return(
         <div>
             <Header />
+            <div className={styles.sectionBanner}>
+                <h1>HotelEasy</h1>
+                <h3>Encontre o lugar perfeito para sua próxima viagem.</h3>
+            </div>
         </div>
     )
 }
