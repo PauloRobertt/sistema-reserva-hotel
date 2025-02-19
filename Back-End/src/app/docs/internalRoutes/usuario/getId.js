d03e643ba6b9182751d1b388cc40371564b82c49
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2025-02-19 16:22:49 -0300

    docs(back-end): Retornar usuario por id

---
+++ Back-End/src/app/docs/internalRoutes/usuario/getId.js
@@ -0,0 +1,41 @@
+export default {
+    get: {
+        tags: ['Usuario'],
+        description: 'Retornar usuario',
+        operationId: 'getUsuario',
+        summary: 'Retornar usuario',
+        parameters: [
+            {
+                in: 'path',
+                name: 'id',
+                required: true,
+                schema: {
+                    type: 'integer',
+                },
+                description: 'Número do id do usuario'
+            }
+        ],
+        responses: {
+            '200': {
+                description: 'Usuario retornado',
+                content: {
+                    'application/json': {
+                        schema: {
+                            $ref: '#/components/schemas/usuarioResponse'
+                        },
+                    },
+                },
+            },
+            '500': {
+                description: 'Erro interno',
+                content: {
+                    'application/json': {
+                        schema: {
+                            $ref: '#/components/schemas/Error'
+                        },
+                    },
+                },
+            },
+        },
+    },
+}
