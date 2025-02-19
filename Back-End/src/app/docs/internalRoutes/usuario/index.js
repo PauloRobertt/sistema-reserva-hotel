a3811eb747baf8dc72a7f85516841cdcbd16faab
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2025-02-19 17:02:34 -0300

    docs(back-end): reestruturado documentos Swagger

--- Back-End/src/app/docs/internalRoutes/usuario/index.js
+++ Back-End/src/app/docs/internalRoutes/usuario/index.js
@@ -1,7 +1,17 @@
-import get from './get.js';
+import get from './metodosHTTP/get.js';
+import getId from './metodosHTTP/getId.js';
+import post from './metodosHTTP/post.js';
+import put from './metodosHTTP/put.js';
+import _delete from './metodosHTTP/delete.js';
 
 export default {
     '/usuario': {
-        ...get
+        ...get,
+        ...post
+    },
+    '/usuario/{id}':{
+        ...getId,
+        ...put,
+        ..._delete
     }
 }
