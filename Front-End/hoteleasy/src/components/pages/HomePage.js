13dcb807f51145e16a37142289347e5febe0da70
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2025-03-07 16:40:42 -0300

    feat(front-end): adiciona listagem de quartos na HomePage

--- Front-End/hoteleasy/src/components/pages/HomePage.js
+++ Front-End/hoteleasy/src/components/pages/HomePage.js
@@ -1,8 +1,28 @@
+import { useEffect, useState } from "react";
+
 import Header from "../layout/Header.js";
+import QuartosCard from "../layout/QuartosCard.js";
+
 import styles from './HomePage.module.css';
 
-export default function HomePage(){
-    return(
+export default function HomePage() {
+    const [quartos, setQuartos] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3001/quarto", {
+            method: "GET",
+            headers: {
+                'Content-type': 'application/json'
+            }
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                setQuartos(data);
+            })
+            .catch((error) => { console.log(error) })
+    }, []);
+
+    return (
         <div>
             <Header />
             <div className={styles.banner}>
@@ -10,6 +30,27 @@ export default function HomePage(){
                 <h2>
                     Encontre o lugar perfeito para sua próxima viagem.
                 </h2>
+            </div>
+            <div className={styles.containerHotels}>
+                <div className={styles.filters}></div>
+                <div className={styles.conteudoHotels}>
+                    <div className={styles.search}></div>
+                    <div className={styles.hotels}>
+                        {quartos.length > 0
+                            ? quartos.map((quarto) => {
+                                return (<QuartosCard
+                                    id={quarto.id}
+                                    imagemUrl={quarto.imagemUrl}
+                                    nomeQuarto={quarto.nomeQuarto}
+                                    preco={quarto.preco}
+                                    idHotel={quarto.idHotel}
+                                    disponibilidade={quarto.disponibilidade}
+                                />)
+                            })
+                            : <p>Nenhum quarto encontrado!</p>
+                        }
+                    </div>
+                </div>
             </div>
         </div>
     )
