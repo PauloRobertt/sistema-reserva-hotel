f6d3f64f37b3f50714c12c164b690583cdf4ee43
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2025-04-01 18:59:52 -0300

    feat: adiciona suporte a refreshToken no login e armazena em cookies

--- Back-End/src/app/controllers/authenticationController.js
+++ Back-End/src/app/controllers/authenticationController.js
@@ -1,13 +1,24 @@
 import service from "../service/authenticationService.js";
+import cookieParser from 'cookie-parser';
 
 class authenticationController {
-    
-    async login(req, res){
+
+    async login(req, res) {
         try {
-            const result = await service.login(req.body);
-            res.status(200).json({message: "Login Success", token: result});
+            const { acessToken, refreshToken } = await service.login(req.body);
+
+            res.cookie("refreshToken", refreshToken, {
+                httpOnly: true,
+                secure: true,
+                sameSite: "Strict",
+                maxAge: 30 * 24 * 60 * 60 * 1000
+            });
+
+            res.status(200).json({
+                message: "Login Success", token: acessToken
+            });
         } catch (error) {
-            return res.status(500).json({message: `${error.message}`});
+            return res.status(500).json({ message: `${error.message}` });
         }
     }
 
@@ -17,7 +28,7 @@ class authenticationController {
             return res.status(200).json(result);
         }
         catch (error) {
-            return res.status(500).json({message: `${error.message}`});
+            return res.status(500).json({ message: `${error.message}` });
         }
     }
 }
