5cc953498c1eaf4792396accb61f682c53d12384
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2025-04-01 18:59:52 -0300

    fix: ajusta configuração do CORS para suportar credenciais

--- Back-End/src/app.js
+++ Back-End/src/app.js
@@ -5,7 +5,10 @@ import cors from 'cors';
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
 app.use(router);
 
 export default app;
