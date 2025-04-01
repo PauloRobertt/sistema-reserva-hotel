02797917140b49c91fa6d0757fbdfa0013f24fe9
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2025-04-01 18:59:53 -0300

    refactor: separa geração de accessToken e refreshToken no TokenService

--- Back-End/src/app/security/TokenService.js
+++ Back-End/src/app/security/TokenService.js
@@ -2,41 +2,55 @@ import 'dotenv/config';
 import jwt from 'jsonwebtoken';
 
 class TokenService {
-    generateToken(user) {
-        const secret = process.env.SECRET;
-        const token = jwt.sign(
+    generateTokens(user) {
+        //const secret = process.env.SECRET;
+
+        const acess_secret = process.env.ACCESS_SECRET;
+        const refresh_secret = process.env.REFRESH_SECRET;
+
+        const refreshToken = jwt.sign(
             {
                 id: user.id,
                 email: user.email,
                 role: user.role
             },
-            secret,
-            { expiresIn: '1h' }
+            refresh_secret,
+            { expiresIn: '7d' }
         );
 
-        return token;
+        const acessToken = jwt.sign(
+            {
+                id: user.id,
+                email: user.email,
+                role: user.role
+            },
+            acess_secret,
+            { expiresIn: '5m' }
+        );
+
+        return { refreshToken, acessToken };
     }
 
     checkToken(req, res, next) {
         try {
             const secret = process.env.SECRET;
 
+            const acess_secret = process.env.ACCESS_SECRET;
+            const refresh_secret = process.env.REFRESH_SECRET;
+
             const authHeader = req.headers['authorization'];
             const token = authHeader && authHeader.split(' ')[1];
 
             if (!token) {
                 res.status(403).json({ msg: 'Token não encontrado!' });
+                return;
             }
 
-            const decode = jwt.verify(token, secret);
-
-            req.user = {
-                id: decode.id, 
-                email: decode.email, 
-                role: decode.role
-            };
-
-            next();
+            jwt.verify(token, acess_secret, (err, user) => {
+                if (err) return res.status(403).json({ message: "Token inválido!" });
+                req.user = user;
+                next();
+            });
 
         } catch (error) {
             res.status(404).json({ msg: 'Token Invalido!' });
