6e7372d2ac093a884be4d28b7fa0088bd3372ad0
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2025-05-14 17:11:59 -0300

    chore(front-end): remoção de imagens teste

--- Front-End/hoteleasy/src/assets/OrganizarImg.js
+++ Front-End/hoteleasy/src/assets/OrganizarImg.js
@@ -1,15 +1,5 @@
-import ChatGptImage1 from './img/ChatGPT Image May 13, 2025, 04_45_01 PM.png';
-import ChatGptImage2 from './img/ChatGPT Image May 13, 2025, 06_23_03 PM.png';
-import ChatGptImage3 from './img/ChatGPT Image May 13, 2025, 06_30_09 PM.png';
-import ChatGptImage4 from './img/ChatGPT Image May 13, 2025, 06_30_14 PM.png';
-import ChatGptImage5 from './img/ChatGPT Image May 13, 2025, 06_30_18 PM.png';
-import ChatGptImage6 from './img/ChatGPT Image May 13, 2025, 08_58_30 PM.png';
+import ImageHotel from './img/HotelImg.png';
 
 export const OrganizarImg = {
-    Image1: ChatGptImage1,
-    Image2: ChatGptImage2,
-    Image3: ChatGptImage3,
-    Image4: ChatGptImage4,
-    Image5: ChatGptImage5,
-    Image6: ChatGptImage6
+    ImageHotel: ImageHotel
 }
