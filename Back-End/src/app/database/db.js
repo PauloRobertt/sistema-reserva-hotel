6f0c6d21bc64072d54e569ee0db42edcba88e505
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2025-05-16 19:33:01 -0300

    chore(back-end): adicao de comando para nao exibir as logs de comandos SQL

--- Back-End/src/app/database/db.js
+++ Back-End/src/app/database/db.js
@@ -4,6 +4,7 @@ import 'dotenv/config';
 const sequelize = new Sequelize('Sistema_de_Reserva', process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'postgres',
+    logging: false
 });
 
 export default sequelize;
