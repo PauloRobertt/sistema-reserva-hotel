57ecb6018015815979891494e3ef190694098177
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2025-05-17 01:51:37 -0300

    feat(front-end): integração do React Toastify na HomePage
    - Adicionada notificação de sucesso ao redirecionar para Home após login.
    - Configurado ToastContainer para exibição das mensagens.

--- Front-End/hoteleasy/src/pages/HomePage/HomePage.jsx
+++ Front-End/hoteleasy/src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,50 @@
 import Header from "../../layout/Header.jsx"
+import { useEffect } from 'react';
+import { ToastContainer, toast, Bounce } from 'react-toastify';
+import { useLocation, useNavigate } from 'react-router-dom';
 
-export default function HomePage(){
-    return(
+
+export default function HomePage() {
+    const navegate = useNavigate();
+    const { state } = useLocation();
+
+    const functionToast = () => {
+        toast.success('Usuario logado com sucesso!', {
+            position: "top-center",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: false,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "colored",
+            transition: Bounce,
+        });
+    }
+
+    useEffect(() => {
+        if (state?.executarFuncao) {
+            functionToast();
+            navegate('.', { replace: true, state: {} });
+        }
+    }, [state]);
+
+    return (
         <div>
             <Header />
+            <ToastContainer
+                position="top-center"
+                autoClose={2000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick={false}
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="colored"
+                transition={Bounce}
+            />
         </div>
     )
 }
