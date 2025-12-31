76fd2fdd4c0809ccd0392c92cc5ed1632e3f6e13
paulorobertt <paulorobert5776@gmail.com>
2025-12-31 18:30:06 -0300

    Criação de um texto titulo e logo

---
+++ Front-End/hoteleasy/src/layout/Header/Header.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+import styles from "./Header.module.css";
+
+//Components
+import Menu from "../Menu.jsx";
+import LinkButton from "../../components/LinkButton.jsx";
+import stylesLinkButton from "../../components/LinkButton.module.css";
+
+import { OrganizarImg } from "../../assets/OrganizarImg.js";
+
+//ReactIcons
+import { SlMenu } from "react-icons/sl";
+
+export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  function toggleMenu() {
+    setIsMenuOpen((prev) => !prev);
+  }
+
+  return (
+    <div className={styles.sectionHeader}>
+      <div className={`maxSection ${styles.conteudoHeader}`}>
+        <div className={styles.headerLogo}>
+          <img src={OrganizarImg.LogoAzul} alt="" />
+          <h1>
+            Hotel<span>Easy</span>
+          </h1>
+        </div>
+        <div className={styles.contentLink}>
+          <nav className={styles.LinksHeader}>
+            <LinkButton
+              text="Início"
+              caminho=""
+              className={stylesLinkButton.LinkHeader}
+            />
+            <LinkButton
+              text="Hoteis"
+              caminho=""
+              className={stylesLinkButton.LinkHeader}
+            />
+            <LinkButton
+              text="Contato"
+              caminho=""
+              className={stylesLinkButton.LinkHeader}
+            />
+          </nav>
+          <div className={styles.iconMenu}>
+            <SlMenu onClick={toggleMenu} size={23} color="#696868" />
+          </div>
+        </div>
+      </div>
+      <Menu stateMenu={isMenuOpen} toggleMenu={toggleMenu} />
+    </div>
+  );
+}
