3b6a34038e8183f1c319a14c68aeb35eba345270
paulorobertt <paulorobert5776@gmail.com>
2026-01-07 21:31:52 -0300

    Card Banner sobre informações da aplicação

--- Front-End/hoteleasy/src/layout/Banner/Banner.jsx
+++ Front-End/hoteleasy/src/layout/Banner/Banner.jsx
@@ -1,4 +1,13 @@
 import styles from "./Banner.module.css";
+import stylesLinkButton from "../../components/LinkButton.module.css";
+
+// Componentes
+import LinkButton from "../../components/LinkButton.jsx";
+
+// React Icons
+import { FaStar } from "react-icons/fa6";
+import { IoCloseCircleOutline } from "react-icons/io5";
+import { IoIosSearch } from "react-icons/io";
 
 export default function Banner() {
   return (
@@ -6,6 +15,36 @@ export default function Banner() {
       <div className={styles.containerText}>
         <h1>Encontre o hotel ideal para sua viagem</h1>
         <p>Compare preços, escolha data reserve em pouco tempo.</p>
+      </div>
+      <div className={styles.containerSaibaMais}>
+        <h2>Reserve com confiança</h2>
+        <ul>
+          <li>
+            <FaStar size="1.5em" color="var(--SecondaryColor)" />
+            <p>
+              Avaliações reais
+              <small>Cancelamento fácil em hotéis selecionados</small>
+            </p>
+          </li>
+          <li>
+            <IoIosSearch size="1.5em" color="var(--SecondaryColor)" />
+            <p>
+              Procura fácil e rápida
+              <small>Busque e compare hotéis em segundos</small>
+            </p>
+          </li>
+          <li>
+            <IoCloseCircleOutline size="1.5em" color="var(--SecondaryColor)" />
+            <p>
+              Cancelamento fácil
+              <small>Cancelamento fácil em hotéis selecionados</small>
+            </p>
+          </li>
+        </ul>
+        <LinkButton
+          className={stylesLinkButton.LinkButtonSaibaMais}
+          text="Saiba Mais"
+        />
       </div>
     </div>
   );
